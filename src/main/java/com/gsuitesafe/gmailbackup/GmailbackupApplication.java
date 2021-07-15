166b18c736444619132a434e2a27040fc6fa84ba
James Freitas <jamesfrj@yahoo.com.br>
2021-07-15 07:53:53 -0300

    First commit

---
+++ src/main/java/com/gsuitesafe/gmailbackup/GmailbackupApplication.java
@@ -0,0 +1,13 @@
+package com.gsuitesafe.gmailbackup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmailbackupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmailbackupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsuitesafe/gmailbackup/GmailbackupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsuitesafe.gmailbackup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmailbackupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
